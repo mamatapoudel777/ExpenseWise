7959cb54f023ce1b7ac31084d3e8650e5420daad
Mamata Poudel <mamatapoudel067@gmail.com>
2025-12-23 16:04:39 +0545

    change folder structure added admin dashboard folder

--- src/components/admindashboard/DashboardLayout.tsx
+++ src/components/admindashboard/DashboardLayout.tsx
@@ -1,16 +1,14 @@
 // components/DashboardLayout.jsx
-import Sidebar from './Sidebar';
-import Header from './Header';
+import Sidebar from '../commoncomponents/Sidebar';
+import Header from '../commoncomponents/Header';
 import DashboardContent from './DashboardContent';
 
 function DashboardLayout() {
   return (
     <div className="flex h-screen bg-[#F7F8FC]">
-      {/* 1. Sidebar */}
       <Sidebar />
 
       <div className="ml-64 flex flex-col w-[calc(100%-16rem)]">
-        {/* Header is sticky on top */}
         <Header />
 
         <div className="flex-1 overflow-y-auto">
--- src/components/admindashboard/UsersList.tsx
+++ src/components/admindashboard/UsersList.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Trash2, Edit } from 'lucide-react';
-import Sidebar from './Sidebar';
-import Header from './Header';
+import Sidebar from '../commoncomponents/Sidebar';
+import Header from '../commoncomponents/Header';
 import DeleteUserModal from './DeleteUserModal';
 import EditUserModal from './EditModule';
 
